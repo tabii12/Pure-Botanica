7b4f7bb0dac6d53b487165b4623382e3fd46b5b2
tabii <tabibito2k512@gmail.com>
2025-03-21 12:24:43 +0700

    ok

--- src/app/state/page.tsx
+++ src/app/state/page.tsx
@@ -2,16 +2,25 @@
 
 import { useState } from 'react';
 import styles from './state.module.css';
+import { FaHeart } from 'react-icons/fa';
 
 export default function Home(){
     const [count, setCount] = useState(0);
+    const [liked, setLiked] = useState(false);
+
     const dec = () => setCount(count - 1);
+    const toggleLike = () => setLiked(!liked);
+
     return (
         <div className={styles.page}>
             <h1>State</h1>
             <p>count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={dec}>Decrement</button>
+            <button  onClick={() => setCount(count + 1)}>Increment</button>
+            <button  onClick={dec}>Decrement</button>
+            <button style={{ color: liked ? 'red' : 'gray' }} onClick={toggleLike}>
+                <FaHeart />
+            </button>
+
         </div>
     );
 }
