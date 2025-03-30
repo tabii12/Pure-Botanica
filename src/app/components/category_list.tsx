891e2862f42f0e82860b359624745f58dadb479b
tabii <tabibito2k512@gmail.com>
2025-03-30 18:28:31 +0700

    show cate

---
+++ src/app/components/category_list.tsx
@@ -0,0 +1,21 @@
+import { Category } from "./category_interface";
+import { SubCategory } from "./sub_category_interface";
+
+export default function CategoryList({ categories, subCategories }: { categories: Category[]; subCategories: SubCategory[] }) {
+    return (
+        <div className="category-list">
+            {categories.map((category) => (
+                <div key={category.id} className="category-item">
+                    <h2>{category.name}</h2>
+                    <ul>
+                        {subCategories
+                            .filter((subCategory) => subCategory.category === category.id)
+                            .map((subCategory) => (
+                                <li key={subCategory.id}>{subCategory.name}</li>
+                            ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
+}
